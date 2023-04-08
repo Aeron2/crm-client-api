9b017cbb096de86b705047f50951c4c4e0b52348
Dhruv Aeron <dhruv.aeron3@gmail.com>
2023-04-08 16:29:57 +0530

    create new ticket

---
+++ src/model/ticket/TicketModel.js
@@ -0,0 +1,14 @@
+const { TicketSchema } = require('./TicketSchema');
+
+exports.insertTicket = (ticketObj) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema(ticketObj)
+        .save()
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
--- src/routers/ticketRouter.js
+++ src/routers/ticketRouter.js
@@ -1,11 +1,48 @@
 const express = require('express');
 
 const router = express.Router();
+const { insertTicket } = require('../model/ticket/TicketModel');
 
 router.all('/', (req, res, next) => {
-  res.json({
-    message: ' return from ticket router ',
-  });
-});
+  // res.json({
+  // message: ' return from ticket router ',
 
+  // });
+  next();
+});
+router.get('/', async (req, res, next) => {
+  //recieve ticket
+  try {
+    const { subject, sender, message } = req.body;
+
+    const ticketObj = {
+      clientId: '64233099aa6980b704603153',
+      subject,
+      conversations: [
+        {
+          sender,
+          message,
+        },
+      ],
+    };
+    const result = await insertTicket(ticketObj);
+    if (result?._id) {
+      return res.json({
+        status: 'success',
+        message: 'New ticket has been creatd successfully',
+      });
+    }
+    res.json({
+      status: 'error',
+      message: 'Unabel to create new ticket try again later',
+    });
+  } catch (error) {
+    res.json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  //add to mongo
+});
 module.exports = router;
